b85417b2080f70b5fdbee6f7ed58d93fec807d0f
adityag7522 <66068085+adityag7522@users.noreply.github.com>
2023-07-28 12:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+const sizeInfoDiv = document.getElementById('sizeInfo')
+
+const resizeHeader = sizeInfoDiv.appendChild(document.createElement('h1'))
+
+let x = innerWidth
+let y = innerHeight
+resizeHeader.innerHTML = `Width: ${x} and Height: ${y}`
+
+window.addEventListener('resize',() => {
+    x = innerWidth
+    y = innerHeight
+    resizeHeader.innerHTML = `Width: ${x} and Height: ${y}`
+})
